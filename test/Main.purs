67baae63570bc3c6206a4a8c83b4d506fb6791b2
Connor Prussin <cprussin@netflix.com>
2017-11-17 18:07:37 -0800

    Add async testing

--- test/Main.purs
+++ test/Main.purs
@@ -1,11 +1,16 @@
 module Test.Main where
 
 import Prelude
+import Control.Monad.Aff (launchAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Performance.Minibench (bench, benchWith)
+import Control.Monad.Aff.Console as AffConsole
+import Node.Encoding (Encoding(UTF8))
+import Node.FS (FS)
+import Node.FS.Aff (readTextFile)
+import Performance.Minibench (bench, benchAff, benchWith, benchAffWith)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: forall e. Eff (console :: CONSOLE, fs :: FS | e) Unit
 main = do
   let loop 0 = 0
       loop n = loop (n - 1)
@@ -22,3 +27,14 @@ main = do
   bench \_ -> loop 100000
   log "loop 1000000"
   benchWith 100 \_ -> loop 1000000
+
+  void $ launchAff do
+    AffConsole.log "bench aff"
+    benchAff $ readTextFile UTF8 "./test/mockFile"
+    AffConsole.log "bench aff triple read"
+    benchAff do
+      _ <- readTextFile UTF8 "./test/mockFile"
+      _ <- readTextFile UTF8 "./test/mockFile"
+      readTextFile UTF8 "./test/mockFile"
+    AffConsole.log "bench aff x10"
+    benchAffWith 10 $ readTextFile UTF8 "./test/mockFile"
