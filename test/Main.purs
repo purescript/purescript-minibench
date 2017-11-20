e8f16d2929f105dce2427d7ba01fe3619a6c075d
Connor Prussin <cprussin@netflix.com>
2017-11-19 22:40:33 -0800

    Test without using purescript-node-fs

--- test/Main.purs
+++ test/Main.purs
@@ -3,15 +3,13 @@ module Test.Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Node.Encoding (Encoding(UTF8))
-import Node.FS (FS)
-import Node.FS.Async (readTextFile)
 import Performance.Minibench (bench, benchAsync, benchAsyncWith, benchWith)
 
-asyncReadFile :: forall e . Eff (fs :: FS | e) Unit -> Eff (fs :: FS | e) Unit
-asyncReadFile done = readTextFile UTF8 "./test/mockFile" \_ -> done
+asyncLoop :: forall e. Int -> Eff e Unit -> Eff e Unit
+asyncLoop 0 done = done
+asyncLoop n done = asyncLoop (n - 1) done
 
-main :: forall e. Eff (console :: CONSOLE, fs :: FS | e) Unit
+main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   let loop 0 = 0
       loop n = loop (n - 1)
@@ -29,9 +27,9 @@ main = do
   log "loop 1000000"
   benchWith 100 \_ -> loop 1000000
 
-  log "bench aff"
-  benchAsync asyncReadFile do
-    log "bench aff x10"
-    benchAsyncWith 10 asyncReadFile do
-      log "bench aff triple read"
-      benchAsync (asyncReadFile >>> asyncReadFile >>> asyncReadFile) $ pure unit
+  log "bench async loop 10"
+  benchAsync (asyncLoop 10) do
+    log "bench async loop 10 x10"
+    benchAsyncWith 10 (asyncLoop 10) do
+      log "bench async loop 1000000"
+      benchAsync (asyncLoop 1000000) $ pure unit
