cccc1e2cd1f098a31275e284391bd7bdc6c69b00
Nathan Faubion <nathan@n-son.com>
2022-08-25 17:00:00 -0700

    Update FFI to be more portable and direct.

--- src/Performance/Minibench.js
+++ src/Performance/Minibench.js
@@ -1,4 +1,9 @@
-export const hrTime = process.hrtime;
+export function timeNs(k) {
+  const t1 = process.hrtime();
+  k();
+  const t2 = process.hrtime(t1);
+  return t2[0] * 1.0e9 + t2[1];
+}
 
 export function gc() {
   global.gc && global.gc();
